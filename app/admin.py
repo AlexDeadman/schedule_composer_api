1f0dcd75be8bcb712f3885c8334a73f9932da0ea
AlexDeadman <alexo.gohome@gmail.com>
2022-03-19 15:19:22 +0300

    Initial commit

---
+++ app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Direction)
+admin.site.register(Syllabus)
+admin.site.register(Discipline)
+admin.site.register(Lecturer)
+admin.site.register(Group)
+admin.site.register(Classroom)
+admin.site.register(Schedule)
---
+++ schedule_composer_api/prod_settings.py
@@ -0,0 +1,23 @@
+# EXAMPLE
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
