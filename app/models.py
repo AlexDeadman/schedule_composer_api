afc1a19f477d54fc6dfff170623ee0d3d7518cec
AlexDeadman <alexo.gohome@gmail.com>
2022-04-03 14:14:57 +0300

    models.py update

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
-from django.db import models
+from django.db.models import *
 
 
 def gte_zero(value):
@@ -20,8 +20,8 @@ def gt_zero(value):
         )
 
 
-class Direction(models.Model):
-    code = models.CharField(
+class Direction(Model):
+    code = CharField(
         max_length=8,
         verbose_name="Код",
         validators=[
@@ -31,7 +31,7 @@ class Direction(models.Model):
             )
         ]
     )
-    name = models.CharField(max_length=100, verbose_name="Название")
+    name = TextField(verbose_name="Название")
 
     def __str__(self):
         return self.code
@@ -41,20 +41,20 @@ class Direction(models.Model):
         verbose_name_plural = "Направления"
 
 
-class Syllabus(models.Model):
-    year = models.CharField(
+class Syllabus(Model):
+    year = CharField(
         max_length=9,
         verbose_name="Год",
         validators=[
             RegexValidator(
-                regex=r"^\d{4}/\d{4}$",
+                regex=r"^\d{4}\/\d{4}$",
                 message="Wrong format"
             )
         ]
     )
-    specialty_code = models.CharField(max_length=100, verbose_name="Код специальности")
-    specialty_name = models.CharField(max_length=100, verbose_name="Название специальности")
-    direction = models.ForeignKey(Direction, on_delete=models.CASCADE, verbose_name="Направление")
+    specialty_code = TextField(verbose_name="Код специальности")
+    specialty_name = TextField(verbose_name="Название специальности")
+    direction = ForeignKey(Direction, on_delete=CASCADE, verbose_name="Направление")
 
     def __str__(self):
         return f"{self.year} {self.specialty_name}"
@@ -64,18 +64,18 @@ class Syllabus(models.Model):
         verbose_name_plural = "Учебные планы"
 
 
-class Discipline(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название")
-    code = models.CharField(max_length=100, verbose_name="Код")
-    syllabus = models.ForeignKey(Syllabus, on_delete=models.CASCADE, verbose_name="Учебный план")
-    cycle = models.CharField(max_length=100, verbose_name="Цикл")
+class Discipline(Model):
+    name = TextField(verbose_name="Название")
+    code = TextField(verbose_name="Код")
+    syllabus = ForeignKey(Syllabus, on_delete=CASCADE, verbose_name="Учебный план")
+    cycle = TextField(verbose_name="Цикл")
 
-    hours_total = models.SmallIntegerField(verbose_name="Всего часов", validators=[gte_zero])
-    hours_lec = models.SmallIntegerField(verbose_name="Лек.", validators=[gte_zero], null=True, blank=True)
-    hours_pr = models.SmallIntegerField(verbose_name="Прак.", validators=[gte_zero], null=True, blank=True)
-    hours_la = models.SmallIntegerField(verbose_name="Лаб.", validators=[gte_zero], null=True, blank=True)
-    hours_isw = models.SmallIntegerField(verbose_name="СРС", validators=[gte_zero], null=True, blank=True)
-    hours_cons = models.SmallIntegerField(verbose_name="Конс.", validators=[gte_zero], null=True, blank=True)
+    hours_total = SmallIntegerField(verbose_name="Всего часов", validators=[gte_zero])
+    hours_lec = SmallIntegerField(verbose_name="Лек.", validators=[gte_zero], null=True, blank=True)
+    hours_pr = SmallIntegerField(verbose_name="Прак.", validators=[gte_zero], null=True, blank=True)
+    hours_la = SmallIntegerField(verbose_name="Лаб.", validators=[gte_zero], null=True, blank=True)
+    hours_isw = SmallIntegerField(verbose_name="СРС", validators=[gte_zero], null=True, blank=True)
+    hours_cons = SmallIntegerField(verbose_name="Конс.", validators=[gte_zero], null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -85,11 +85,11 @@ class Discipline(models.Model):
         verbose_name_plural = "Дисциплины"
 
 
-class Lecturer(models.Model):
-    first_name = models.CharField(max_length=30, verbose_name="Имя")
-    surname = models.CharField(max_length=30, verbose_name="Фамилия")
-    patronymic = models.CharField(max_length=30, null=True, blank=True, verbose_name="Отчество")
-    disciplines = models.ManyToManyField(Discipline, verbose_name="Дисциплины")
+class Lecturer(Model):
+    first_name = TextField(verbose_name="Имя")
+    surname = TextField(verbose_name="Фамилия")
+    patronymic = TextField(null=True, blank=True, verbose_name="Отчество")
+    disciplines = ManyToManyField(Discipline, verbose_name="Дисциплины")
 
     def __str__(self):
         return f"{self.surname} {self.first_name} {self.patronymic or ''}"
@@ -99,10 +99,19 @@ class Lecturer(models.Model):
         verbose_name_plural = "Преподаватели"
 
 
-class Group(models.Model):
-    number = models.CharField(max_length=5, verbose_name="Номер")
-    students_count = models.IntegerField(verbose_name="Количество студентов", validators=[gte_zero])
-    syllabus = models.ForeignKey(Syllabus, on_delete=models.CASCADE, verbose_name="Учебный план")
+class Group(Model):
+    number = CharField(
+        max_length=5,
+        verbose_name="Номер",
+        validators=[
+            RegexValidator(
+                regex=r"^[a-zA-Zа-яА-ЯёЁ]{1}\d{4}$",
+                message="Wrong format"
+            )
+        ]
+    )
+    students_count = IntegerField(verbose_name="Количество студентов", validators=[gte_zero])
+    syllabus = ForeignKey(Syllabus, on_delete=CASCADE, verbose_name="Учебный план")
 
     def __str__(self):
         return self.number
@@ -112,8 +121,8 @@ class Group(models.Model):
         verbose_name_plural = "Группы"
 
 
-class Classroom(models.Model):
-    number = models.CharField(
+class Classroom(Model):
+    number = CharField(
         max_length=3,
         verbose_name="Номер",
         unique=True,
@@ -124,8 +133,8 @@ class Classroom(models.Model):
             )
         ]
     )
-    type = models.CharField(max_length=30, null=True, blank=True, verbose_name="Тип аудитории")
-    seats_count = models.SmallIntegerField(verbose_name="Количество мест", validators=[gte_zero])
+    type = TextField(null=True, blank=True, verbose_name="Тип аудитории")
+    seats_count = SmallIntegerField(verbose_name="Количество мест", validators=[gte_zero])
 
     def __str__(self):
         return self.number
@@ -161,17 +170,17 @@ LECTURE_TYPE = (
 )
 
 
-class Schedule(models.Model):
-    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE, verbose_name="Преподаватель")
-    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE, verbose_name="Дисциплина")
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Группа")
-    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, verbose_name="Аудитория")
-    lecture_type = models.SmallIntegerField(choices=LECTURE_TYPE, verbose_name="Тип занятия")
+class Schedule(Model):
+    lecturer = ForeignKey(Lecturer, on_delete=CASCADE, verbose_name="Преподаватель")
+    discipline = ForeignKey(Discipline, on_delete=CASCADE, verbose_name="Дисциплина")
+    group = ForeignKey(Group, on_delete=CASCADE, verbose_name="Группа")
+    classroom = ForeignKey(Classroom, on_delete=CASCADE, verbose_name="Аудитория")
+    lecture_type = SmallIntegerField(choices=LECTURE_TYPE, verbose_name="Тип занятия")
 
-    semester = models.SmallIntegerField(verbose_name="Семестр", validators=[gt_zero])
-    week_parity = models.BooleanField(verbose_name="Неделя чёт/нечёт")
-    day_of_the_week = models.SmallIntegerField(choices=DAYS_OF_WEEK, verbose_name="День недели")
-    lecture_begin = models.SmallIntegerField(choices=LECTURE_BEGIN, verbose_name="Начало пары")
+    semester = SmallIntegerField(verbose_name="Семестр", validators=[gt_zero])
+    week_parity = BooleanField(verbose_name="Неделя чёт/нечёт")
+    day_of_the_week = SmallIntegerField(choices=DAYS_OF_WEEK, verbose_name="День недели")
+    lecture_begin = SmallIntegerField(choices=LECTURE_BEGIN, verbose_name="Начало пары")
 
     def __str__(self):
         return f"Семестр: {self.semester} Группа: {self.group} День: {self.day_of_the_week} Пара: {self.lecture_begin}"
